07439098cf4838057754fd7dd991532329afccbf
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-04-23 21:12:46 +0800

    Create logic to find how many steps (counts) to get to the end position

--- knightsTravails.js
+++ knightsTravails.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 
-function knightMoves(start = [7,0], end = [1,2]) {
+function knightMoves(start = [0, 0], end = [7, 7]) {
     let queue = [start]; 
     const noRepeatvertex = [start];
+    let count = 0;
     while (true) {
+        count += 1;
         let newQueue = [];
         for (const i of queue) {
-            const eachMoveArr = eachMove(i);
+            const eachMoveArr = eachMove(i, end);
+            if (eachMoveArr[0] === end[0] && eachMoveArr[1] === end[1]) {
+                return console.log(`Succeed! count: ${count}`)
+            };
             for (const i of eachMoveArr) {
                 const exists = noRepeatvertex.some(arr => arr[0] === i[0] && arr[1] === i[1]);
                 if (exists === false) {
@@ -18,13 +23,16 @@ function knightMoves(start = [7,0], end = [1,2]) {
         queue = newQueue
     };
 
-    function eachMove(vertex) {
+    function eachMove(vertex, end) {
         const move = [[1, 2], [2, 1], [-1, 2], [2, -1], [1, -2], [-2, 1], [-1, -2], [-2, -1]];
         const newVertex = [];
         for (const i of move) {
             const x = vertex[0] + i[0];
             const y = vertex[1] + i[1];
             if (0 <= x && x <= 7 && 0 <= y && y <= 7) {
+                if (x === end[0] && y === end[1]) {
+                    return [x,y]
+                };
                 newVertex.push([x, y])
             }
         }
