f5c6f7c7f984884cb442c19271e0680fa39066d1
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-04-23 17:28:18 +0800

    Prevent revisiting the same position through different paths

--- knightsTravails.js
+++ knightsTravails.js
@@ -1,7 +1,22 @@
 #!/usr/bin/env node
 
 function knightMoves(start = [7,0], end = [1,2]) {
-    eachMove(start);
+    let queue = [start]; 
+    const noRepeatvertex = [start];
+    while (true) {
+        let newQueue = [];
+        for (const i of queue) {
+            const eachMoveArr = eachMove(i);
+            for (const i of eachMoveArr) {
+                const exists = noRepeatvertex.some(arr => arr[0] === i[0] && arr[1] === i[1]);
+                if (exists === false) {
+                    noRepeatvertex.push(i);
+                    newQueue.push(i)
+                }
+            }
+        }
+        queue = newQueue
+    };
 
     function eachMove(vertex) {
         const move = [[1, 2], [2, 1], [-1, 2], [2, -1], [1, -2], [-2, 1], [-1, -2], [-2, -1]];
