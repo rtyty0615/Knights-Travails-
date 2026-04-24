1fbf27746c1f46157b2680a16514ee4e3a2f96e1
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-04-24 16:03:12 +0800

    Revise to keep helper function eachMove() clean and do one thing only

--- knightsTravails.js
+++ knightsTravails.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-function knightMoves(start = [0, 0], end = [7, 7]) {
+function knightMoves(start = [0, 0], end = [7,7]) {
     if (start[0] === end[0] && start[1] === end[1]) {
         return [start]
     };
@@ -9,38 +9,35 @@ function knightMoves(start = [0, 0], end = [7, 7]) {
     while (true) {
         let newQueue = [];
         for (const i of queue) {
-            const eachMoveArr = eachMove(i, end);
-            if (eachMoveArr[0] === end[0] && eachMoveArr[1] === end[1]) {
-                const previousMove = knightMoves(start, i);
-                let printPath = [];
-                for (const k of previousMove) {
-                    printPath.push(k);
+            const eachMoveArr = eachMove(i);
+            for (const j of eachMoveArr) {
+                if (j[0] === end[0] && j[1] === end[1]) {
+                    const previousMove = knightMoves(start, i);
+                    let printPath = [];
+                    for (const k of previousMove) {
+                        printPath.push(k);
+                    };
+                    printPath.push(end);
+                    return printPath
+
                 };
-                printPath.push(end);
-                return printPath
-                
-            };
-            for (const i of eachMoveArr) {
-                const exists = noRepeatvertex.some(arr => arr[0] === i[0] && arr[1] === i[1]);
+                const exists = noRepeatvertex.some(arr => arr[0] === j[0] && arr[1] === j[1]);
                 if (exists === false) {
-                    noRepeatvertex.push(i);
-                    newQueue.push(i)
+                    noRepeatvertex.push(j);
+                    newQueue.push(j)
                 }
             }
         }
         queue = newQueue
     };
 
-    function eachMove(vertex, end) {
+    function eachMove(vertex) {
         const move = [[1, 2], [2, 1], [-1, 2], [2, -1], [1, -2], [-2, 1], [-1, -2], [-2, -1]];
         const newVertex = [];
         for (const i of move) {
             const x = vertex[0] + i[0];
             const y = vertex[1] + i[1];
             if (0 <= x && x <= 7 && 0 <= y && y <= 7) {
-                if (x === end[0] && y === end[1]) {
-                    return [x, y]
-                };
                 newVertex.push([x, y])
             }
         }
@@ -48,5 +45,5 @@ function knightMoves(start = [0, 0], end = [7, 7]) {
     }
 }
 
-const printMoves = knightMoves([0,1], [4,2]);
+const printMoves = knightMoves();
 console.log(printMoves)
